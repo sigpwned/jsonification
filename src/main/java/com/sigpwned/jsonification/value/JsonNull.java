be40e68afca053a634575ac3501a682aef56df42
sigpwned <aboothe@wcgworld.com>
2015-09-13 23:06:20 -0500

    added license to source files

--- src/main/java/com/sigpwned/jsonification/value/JsonNull.java
+++ src/main/java/com/sigpwned/jsonification/value/JsonNull.java
@@ -10,7 +10,21 @@ import com.sigpwned.jsonification.value.scalar.JsonBoolean;
 import com.sigpwned.jsonification.value.scalar.JsonNumber;
 import com.sigpwned.jsonification.value.scalar.JsonString;
 
-
+/**
+ * Copyright 2015 Andy Boothe
+ *     
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 public interface JsonNull extends JsonBoolean, JsonNumber, JsonString, JsonObject, JsonArray {
     public static final JsonNull NULL=new JsonNull() {
         public Boolean getBooleanValue() {
